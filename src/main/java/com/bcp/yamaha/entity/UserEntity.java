c261bd1e31a205cfb3f04978b0268fb001d1628f
13hoomika <bhoomikacp.xworkz@gmail.com>
2025-05-23 12:28:13 +0530

    Added RestController & JS validation for email and phone & Disable submit button if fields are empty

--- src/main/java/com/bcp/yamaha/entity/UserEntity.java
+++ src/main/java/com/bcp/yamaha/entity/UserEntity.java
@@ -25,6 +25,10 @@ import java.util.List;
         @NamedQuery(name = "findByScheduleType", query = "SELECT u FROM UserEntity u WHERE u.scheduleType =: scheduleType"),
         @NamedQuery(name = "updatePassword", query = "UPDATE UserEntity u SET u.password = :password WHERE u.userEmail = :email"),
         @NamedQuery(name = "deleteUser", query = "DELETE FROM UserEntity u WHERE u.userId = :id"),
+        @NamedQuery(name = "emailExist", query = "SELECT COUNT(u) FROM UserEntity u WHERE LOWER(u.userEmail) = LOWER(:email)"),
+        @NamedQuery(name = "phExist", query = "SELECT COUNT(u) FROM UserEntity u WHERE u.userPhoneNumber = :phNumber"),
+//        @NamedQuery(name = "DLExist", query = "SELECT COUNT(u) FROM UserEntity u WHERE u.drivingLicenseNumber = :dlNo")
+
 //        @NamedQuery(name = "updateAccountLockStatus", query = "UPDATE UserEntity u SET u.accountLocked = :accountLocked WHERE u.userEmail = :userEmail"),
 //        @NamedQuery(name = "updateLoginAttemptData", query = "UPDATE UserEntity u SET u.invalidLogInCount = :invalidLogInCount, u.lastLogIn = :lastLogIn WHERE u.userEmail = :userEmail ")
 })
