c1340c6ef2cdf4d0517fd048e8145010396b8f48
13hoomika <bhoomikacp.xworkz@gmail.com>
2025-05-27 12:41:10 +0530

    made AuditEntity as @MappedSuperclass

--- src/main/java/com/bcp/yamaha/entity/AuditEntity.java
+++ src/main/java/com/bcp/yamaha/entity/AuditEntity.java
@@ -1,14 +1,18 @@
 package com.bcp.yamaha.entity;
 
+import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
+@Data
+@MappedSuperclass
 public class AuditEntity {
     @CreationTimestamp
-    private LocalDateTime createdDate;
+    private LocalDateTime createdDateTime = LocalDateTime.now();
 
     @UpdateTimestamp
-    private LocalDateTime lastModifiedDate;
+    private LocalDateTime lastModifiedDateTime;
 }
