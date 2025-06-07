7b84baac598d46d8c32254125da8de6f15d9db2f
13hoomika <bhoomikacp.xworkz@gmail.com>
2025-06-07 21:56:56 +0530

    Replaced System.out.println with logger in UserRestController

--- src/main/java/com/bcp/yamaha/restcontroller/UserRestController.java
+++ src/main/java/com/bcp/yamaha/restcontroller/UserRestController.java
@@ -2,12 +2,17 @@ package com.bcp.yamaha.restcontroller;
 
 import com.bcp.yamaha.service.user.UserService;
 import com.bcp.yamaha.util.ValidationUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@org.springframework.web.bind.annotation.RestController
+@RestController
+@RequestMapping("/user")
+@Slf4j
 public class UserRestController {
     @Autowired
     UserService userService;
@@ -15,6 +20,7 @@ public class UserRestController {
     @GetMapping("/checkUserName/{userName}")
     public String validateUserName(@PathVariable String userName) {
         System.out.println("Received userName: [" + userName + "]");
+        log.debug("Received userName: [{}]", userName);
 
         if (!ValidationUtil.isValidUserName(userName)) {
             return "Invalid name format. Each word must start with uppercase followed by lowercase letters (e.g., John Doe)";
@@ -26,12 +32,13 @@ public class UserRestController {
     @GetMapping(value = "/checkEmailValue/{email:.+}")
     public String getEmailCount(@PathVariable String email) {
         System.out.println("Received email in Controller: [" + email + "]");
+        log.debug("Received email in Controller: [{}]", email);
         if (!ValidationUtil.isValidEmail(email)){
             return "Invalid email format";
         }
 
         boolean isEmailExist = userService.existByEmail(email);
-        System.out.println("isEmailExist: " + isEmailExist);
+        log.debug("isEmailExist: {}", isEmailExist);
 
         return isEmailExist ? "email already exists" : "";
     }
@@ -39,21 +46,22 @@ public class UserRestController {
     @GetMapping(value = "/checkPhValue/{phNo}", produces = MediaType.APPLICATION_JSON_VALUE)
     public String getPhCountForRegister(@PathVariable String phNo){
         System.out.println("Received phNo in Controller: [" + phNo + "]");
+        log.debug("Received phNo in Controller: [{}]", phNo);
 
         if (ValidationUtil.isValidPhoneNumber(phNo)){
             return "Invalid phone number format. phone number must be 10 digits and start with 9, 8, 7, or 6";
         }
         boolean isPhExist = userService.existByPhNumber(phNo);
-        System.out.println("isPhExist: "+ isPhExist);
-        if(isPhExist){
-            return "phone number already exists";
-        }
-        return "";
+        log.debug("isPhExist: {}", isPhExist);
+
+        return ResponseEntity.ok(isPhExist? "phone number already exists" : "");
+
     }
 
     @GetMapping(value = "/checkDlNumber/{dlNo}", produces = MediaType.APPLICATION_JSON_VALUE)
     public String checkDrivingLicenseNumberForRegister(@PathVariable String dlNo) {
         System.out.println("Received DL Number: [" + dlNo + "]");
+        log.debug("Received DL Number: [{}]", dlNo);
 
         // Validate format using regex (KA0120231234567 format)
         if (ValidationUtil.isValidDl(dlNo)) {
@@ -72,6 +80,7 @@ public class UserRestController {
     @GetMapping(value = "/checkPhValue/{phNo}/{currentPh}", produces = MediaType.APPLICATION_JSON_VALUE)
     public String getPhCountForUpdate(@PathVariable String phNo, @PathVariable String currentPh) {
         System.out.println("Received phNo: [" + phNo + "], CurrentPh: [" + currentPh + "]");
+        log.debug("Received phNo: [{}], CurrentPh: [{}]", phNo, currentPh);
 
         if (ValidationUtil.isValidPhoneNumber(phNo)) {
             return "Invalid phone number, must be 10 digits and start with 9, 8, 7, or 6";
@@ -89,6 +98,7 @@ public class UserRestController {
     @GetMapping(value = "/checkDlNumber/{dlNo}/{currentDlNo}", produces = MediaType.APPLICATION_JSON_VALUE)
     public String checkDrivingLicenseNumberForUpdate(@PathVariable String dlNo, @PathVariable String currentDlNo) {
         System.out.println("Received DL Number: [" + dlNo + "], Current DL Number: [" + currentDlNo + "]");
+        log.debug("Received DL Number: [{}], Current DL Number: [{}]", dlNo, currentDlNo);
 
         // Format validation (e.g., KA0120231234567)
         if (ValidationUtil.isValidDl(dlNo)) {
