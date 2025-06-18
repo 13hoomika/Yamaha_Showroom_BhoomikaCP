4187916a7875b92ef8d6b54f7b0f3b072b3c5d0e
13hoomika <bhoomikacp.xworkz@gmail.com>
2025-06-18 22:25:16 +0530

    fix: improved bike model validation regex to support names like 'Ray ZR 125' and 'FZ-S FI Ver 4.0'

--- src/main/java/com/bcp/yamaha/util/ValidationUtil.java
+++ src/main/java/com/bcp/yamaha/util/ValidationUtil.java
@@ -14,8 +14,13 @@ public class ValidationUtil {
     private static final String DL_REGEX = "^[A-Z]{2}\\d{2}\\d{4}\\d{7}$";
 //    private static final String DL_REGEX = "^[A-Z]{2}[0-9]{2}[0-9]{4}[0-9]{7}$";
 
-    private static final String BIKE_MODEL_REGEX =
+    /*private static final String BIKE_MODEL_REGEX =
             "^([A-Z][a-z0-9]{0,19})(?:[- ]([A-Z]{2,}|[A-Z][a-z0-9]{0,19}|\\d{1,4})){0,5}(?:\\s(V\\d+(\\.\\d+)?|Ver\\s\\d+(\\.\\d+)?|FI(\\sHybrid)?|DLX(\\sVer\\s\\d+(\\.\\d+)?)?|Hybrid|S))?$";
+*/
+
+    private static final String BIKE_MODEL_REGEX =
+            "^[A-Z][A-Za-z0-9]*(?:[- ][A-Z][A-Za-z0-9]*|[- ]\\d+(?:\\.\\d+)?){0,6}$";
+
 
     // === Validation Methods ===
     public static String validatePassword(String password) {
@@ -74,7 +79,7 @@ public class ValidationUtil {
         if (!dlNo.matches(DL_REGEX)) {
             return "Please enter a valid Driving License number. It should be 15" +
                     " characters long and follow the pattern: 2 letters (state code), 2 digits (year), " +
-                    "4 digits (unique number), and 7 digits (personal ID). For example: KA0120231234567.";
+                    "4 digits (unique number), and 7 digits (personal ID). (e.g., KA0120231234567)";
             /*return "Please enter a valid Driving License number. It should be " + DL_NUMBER_LENGTH + " characters long.\n" +
                     "• First 2 characters: State code (e.g., KA for Karnataka)\n" +
                     "• Next 2 digits: Issuance year (e.g., 01 for 2001)\n" +
@@ -104,8 +109,8 @@ public class ValidationUtil {
         if (bikeModel.length() > MAX_FIELD_LENGTH)
             return "Bike model cannot exceed " + MAX_FIELD_LENGTH + " characters";
         if (!bikeModel.matches(BIKE_MODEL_REGEX))
-            return "Invalid bike model. Allowed formats: 'Ray Z', 'Ray ZR 125', 'FZ-S FI Ver 4.0'";
-        return null; // Valid
+            return "Invalid bike model. (e.g., R15, Ray ZR 125, FZ-S FI Ver 4.0)";
+        return null;
     }
 
     public static String validateBikeColor(String bikeColor) {
